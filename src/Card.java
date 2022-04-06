fdefe51099014c19e15d525d0bf32e6b0644aa88
cainarce <48627601+cainarce@users.noreply.github.com>
2022-04-05 23:13:57 -0600

    Correccion de conflictos

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 public class Card {
 
     String palo, color, valor;
 
 }
-=======
-public class Card {
-
-    String palo, color, valor;
-
-}
->>>>>>> 6e3068a59020cd5679b963e1967aae123bda23dc
